9cfc851ee1d705c32a190c5ac3000052dd9f05c5
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-10-09 20:01:33 -0300

    remove: currentPage state

--- src/components/AddBookModal/AddBookModal.jsx
+++ src/components/AddBookModal/AddBookModal.jsx
@@ -3,94 +3,95 @@ import propTypes from 'prop-types'
 import styles from "./addbookmodal.module.css"
 export default function AddBookModal({ setModal, addToDB }) {
 
-    const [title, setTitle] = useState("")
-    const [author, setAuthor] = useState("")
-    const [cover, setCover] = useState("")
-    const [pages, setPages] = useState("")
-    const [status, setStatus] = useState("")
-    const [currentPage, setCurrentPage] = useState("")
+	const [title, setTitle] = useState("")
+	const [author, setAuthor] = useState("")
+	const [cover, setCover] = useState("")
+	const [pages, setPages] = useState("")
+	const [status, setStatus] = useState("")
 
-    function handleDrop() {
-        setModal(false)
-    }
+	function handleDrop() {
+		setModal(false)
+	}
 
-    function handleAdd() {
-        const currentBook = ({
-            "title": title,
-            "author": author,
-            "cover": cover,
-            "pages": pages,
-            "status": status,
-            "currentPage": currentPage
-        })
+	function handleAdd() {
+		const currentBook = ({
+			"title": title,
+			"author": author,
+			"cover": cover,
+			"pages": pages,
+			"status": status,
+		})
 
-        addToDB(currentBook)
-        setModal(false)
-    }
+		addToDB(currentBook)
+		setModal(false)
+	}
 
-    return (
-        <div
-            className={styles.container}
-            id={"modal"}
-            onClick={(event) => {
-                if (event.target.id === "modal") {
-                    setModal(false)
-                }
-            }}>
-            <div>
-                <h1>Adicione um Livro</h1>
-                <form>
-                    <label htmlFor="">Titulo do Livro</label>
-                    <input
-                        type="text"
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
+	return (
+		<div
+			className={styles.container}
+			id={"modal"}
+			onClick={(event) => {
+				if (event.target.id === "modal") {
+					setModal(false)
+				}
+			}}>
+			<div>
+				<h1>Adicionar Livro</h1>
+				<form>
+					<div>
+						<label htmlFor="">Titulo</label>
+						<input
+							type="text"
+							onChange={(e) => setTitle(e.target.value)}
+						/>
+					</div>
 
-                    <label htmlFor="">Autor do Livro</label>
-                    <input
-                        type="text"
-                        onChange={(e) => setAuthor(e.target.value)}
-                    />
+					<div>
+						<label htmlFor="">Autor</label>
+						<input
+							type="text"
+							onChange={(e) => setAuthor(e.target.value)}
+						/>
+					</div>
 
-                    <label htmlFor="">Capa do Livro</label>
-                    <input
-                        type="text"
-                        onChange={(e) => setCover(e.target.value)}
-                    />
+					<div>
+						<label htmlFor="">Capa URL</label>
+						<input
+							type="text"
+							onChange={(e) => setCover(e.target.value)}
+						/>
+					</div>
 
-                    <label htmlFor="">Número de Páginas</label>
-                    <input
-                        type="number"
-                        min={0}
-                        onChange={(e) => setPages(e.target.value)}
-                    />
+					<div>
+						<label htmlFor="">Número de Páginas</label>
+						<input
+							type="number"
+							min={0}
+							onChange={(e) => setPages(e.target.value)}
+						/>
+					</div>
 
-                    <label htmlFor="">Página Atual</label>
-                    <input
-                        type="number"
-                        min={0}
-                        onChange={(e) => setCurrentPage(e.target.value)}
-                    />
+					<div>
+						<label htmlFor="">Status de Leitura</label>
+						<select name="" id="" onChange={(e) => setStatus(e.target.value)}>
+							<option value="" defaultValue>Selecione</option>
+							<option value="Start">Começar</option>
+							<option value="Reading">Lendo</option>
+							<option value="Finished">Terminado</option>
+						</select>
+					</div>
 
-                    <label htmlFor="">Status de Leitura</label>
-                    <select name="" id="" onChange={(e) => setStatus(e.target.value)}>
-                        <option value="" selected>Selecione</option>
-                        <option value="Start">Começar</option>
-                        <option value="Reading">Lendo</option>
-                        <option value="Finished">Terminado</option>
-                    </select>
-
-                    <div>
-                        <button type="button" onClick={handleAdd}>ADICIONAR</button>
-                        <button type="button" onClick={handleDrop}>CANCELAR</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+					<div className={styles.buttonWrap}>
+						<button type="button" onClick={handleAdd}>ADICIONAR</button>
+						<button type="button" onClick={handleDrop}>CANCELAR</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	)
 }
 
 AddBookModal.propTypes = {
-    setModal: propTypes.func,
-    addToDB: propTypes.func
+	setModal: propTypes.func,
+	addToDB: propTypes.func
 }
