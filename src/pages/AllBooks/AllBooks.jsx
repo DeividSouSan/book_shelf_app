b7c1d26fe74568fe34bbd6087123a879d5d29e1a
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-10-11 08:56:38 -0300

    fix: handling new firebase structure needs

--- src/pages/AllBooks/AllBooks.jsx
+++ src/pages/AllBooks/AllBooks.jsx
@@ -5,74 +5,29 @@ import RemoveBookModal from "../../components/RemoveBookModal/RemoveBookModal"
 import { BsXSquareFill, BsFillPlusSquareFill } from 'react-icons/bs'
 import styles from "./allbooks.module.css"
 
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
-import { getDatabase, ref, push, onValue, remove, update } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js'
-import { getAuth, signInAnonymously } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js'
-
-import { firebaseConfig } from "../../../controller/FirebaseConfig.js"
+import {authenticate, addToDB, removeFromDB, updateDB} from '../../../controller/FirebaseControl.js';
 
 export default function AllBooks() {
-	const app = initializeApp(firebaseConfig)
-	const database = getDatabase(app)
-
-	const [user, setUser] = useState()
+	const [allBooksFromCurrentUser, setAllBooksFromCurrentUser] = useState();
 
 	const [addBookModalStatus, setAddBookModalStatus] = useState(false)
 	const [removeBookModalStatus, setRemoveBookModalStatus] = useState(false)
 
-	const [allBooksFromCurrentUser, setAllBooksFromCurrentUser] = useState([]);
-
-	function addToDB(newBookInfo) {
-		const books = ref(database, `${user}/books`)
-		push(books, newBookInfo)
-	}
-
-	function removeFromDB(bookID) {
-		const bookPosition = ref(database, `${user}/books/${bookID}`)
-		remove(bookPosition)
-	}
-
-	function updateDB(bookID, updateInfo) {
-		const bookPosition = ref(database, `${user}/books/${bookID}`)
-		const updates = { ...updateInfo }
-		update(bookPosition, updates)
-	}
-
 	useEffect(() => {
-		const auth = getAuth();
-
-		signInAnonymously(auth)
-
-		auth.onAuthStateChanged((user) => {
-			onValue(ref(database, `${user.uid}/books`), (snapshot) => {
-				const data = snapshot.val()
-				let dataArray;
-				if (data) {
-					dataArray = Object.entries(data) /* [[id], [properties]] */
-				} else {
-					dataArray = "";
-				}
-				setAllBooksFromCurrentUser(dataArray)
-			})
-
-			setUser(user.uid)
+		authenticate().then((data) => {
+			setAllBooksFromCurrentUser(data)
 		})
-	}, [user])
+	}, [addBookModalStatus, removeBookModalStatus])
 
 	return (
 		<>
 			<header>
 				<h1 className={styles.pageTitle}>Estante de Livros</h1>
 				<div className={styles.btnWrap}>
-					<button
-						onClick={() => setAddBookModalStatus(!addBookModalStatus)}
-					>
+					<button onClick={() => setAddBookModalStatus(!addBookModalStatus)}>
 						<BsFillPlusSquareFill />Adicionar
 					</button>
-
-					<button
-						onClick={() => setRemoveBookModalStatus(!removeBookModalStatus)}
-					>
+					<button onClick={() => setRemoveBookModalStatus(!removeBookModalStatus)}>
 						<BsXSquareFill />Remover
 					</button>
 				</div>
@@ -80,7 +35,7 @@ export default function AllBooks() {
 
 			<div className={styles.bookWrap}>
 				{
-					allBooksFromCurrentUser ? allBooksFromCurrentUser.map((book, index) => (
+					allBooksFromCurrentUser ? Object.entries(allBooksFromCurrentUser).map((book, index) => (
 						<Card
 							key={index}
 							bookID={book[0]} /* [id] */
@@ -97,11 +52,10 @@ export default function AllBooks() {
 			}
 
 			{
-				removeBookModalStatus && (
-					<RemoveBookModal
+				removeBookModalStatus && <RemoveBookModal
 						setModalStatus={setRemoveBookModalStatus}
 						removeFromDB={removeFromDB}
-						bookOptions={allBooksFromCurrentUser} />)
+						bookOptions={allBooksFromCurrentUser} />
 			}
 		</>
 	)
