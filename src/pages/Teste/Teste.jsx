3cfcc65da0c9616b167736354cad0ae44c700bb6
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-10-16 10:23:23 -0300

    test: jsx page to test new select component

---
+++ src/pages/Teste/Teste.jsx
@@ -0,0 +1,11 @@
+import Select from "../../components/Select/Select";
+
+export default function Teste() {
+    return (
+        <Select defaultOption={'Status'} options={['start', 'reading', 'finished']}>
+            <p>Start</p>
+            <p>Start</p>
+            <p>Start</p>
+        </Select>
+    )
+}
