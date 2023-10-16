ab9d3882b6a6c54c84f212c4877d98782694a704
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-10-16 10:25:02 -0300

    chore: from <home/> to <teste/>

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import Home from './pages/Home/Home'
 
 import "./global.css"
+import Teste from './pages/Teste/Teste'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <Teste />
   </React.StrictMode>,
 )
